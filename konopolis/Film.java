47fddee3328e0028ceac51a79cd15b40657c1bc1
iXaoh <sebastienhals@hotmail.com>
2016-11-10 10:58:57 +0100

    Update Film.java

--- konopolis/Film.java
+++ konopolis/Film.java
@@ -9,7 +9,7 @@ public class Film  {
 	private String [] casting;
 	private int temps;
 	private String langue;
-	
+	private double prix;	
 	public String getTitre() {
 		return Titre;
 	}
@@ -58,5 +58,23 @@ public class Film  {
 	public void setLangue(String langue) {
 		this.langue = langue;
 	}
-	
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}	
+		public Film(String titre, String description, String[] genre, String dateSeance, String real, String[] casting,
+			int temps, String langue,double prix) {
+		super();
+		Titre = titre;
+		this.description = description;
+		this.genre = genre;
+		this.dateSeance = dateSeance;
+		this.real = real;
+		this.casting = casting;
+		this.temps = temps;
+		this.langue = langue;
+		this.prix = prix;
+	}
 }
