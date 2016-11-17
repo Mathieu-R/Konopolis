0089b6cf8a5bd597ce68f31d5cdcdc618504dc42
Mathieu-R <mathieu0709@gmail.com>
2016-11-17 13:33:20 +0100

    fix sql

--- BDD/Konopolis.sql
+++ BDD/Konopolis.sql
@@ -6,35 +6,35 @@ CREATE table tbMovies (
 	time int,
 	language_id int,
 	price double
-);
+)
 
 -- Associations avec la table Movies --
 
 CREATE table tbGenres (
 	genre_id int auto_increment PRIMARY KEY,
 	genre CHAR(30)
-);
+)
 
 CREATE table tbMoviesGenres (
 	movie_genre_id int auto_increment PRIMARY KEY,
 	movie_id int,
 	genre_id int,
-	FOREIGN KEY (movie_id) REFERENCES tbMovies (movie_id) on update cascade,
-	FOREIGN KEY (genre_id) REFERENCES tbMovies (genre_id) on update cascade
-);
+	FOREIGN KEY (movie_id) REFERENCES tbMovies(movie_id) ON UPDATE CASCADE,
+	FOREIGN KEY (genre_id) REFERENCES tbMovies(genre_id) ON UPDATE CASCADE
+)
 
 CREATE table tbCasts (
 	cast_id int auto_increment PRIMARY KEY,
 	cast CHAR(30)	
-);
+)
 
 CREATE table tbMoviesCasts (
 	movie_cast_id int auto_increment PRIMARY KEY,
 	movie_id int,
 	cast_id int,	
-	FOREIGN KEY (movie_id) REFERENCES tbMovies (movie_id) on update cascade,
-	FOREIGN KEY (cast_id) REFERENCES tbCasts (cast_id) on update cascade 
-);
+	FOREIGN KEY (movie_id) REFERENCES tbMovies(movie_id) ON UPDATE CASCADE,
+	FOREIGN KEY (cast_id) REFERENCES tbCasts(cast_id) ON UPDATE CASCADE
+)
 
 -- fin --
 
@@ -43,36 +43,36 @@ CREATE table tbRooms (
 	movie_id int NOT NULL,
 	rows int, 
 	seats_by_row int,
-	incomes: double,
+	incomes double,
 	CONSTRAINT chkRow CHECK (rows between 1 and 20),
 	CONSTRAINT chkColumn CHECK (seats_by_row between 1 and 35),
-	FOREIGN KEY (movie_id) REFERENCES tbMovies (movie_id) ) on update cascade	
-);
-
-CREATE table tbSeats (
-	seat_id int auto_increment PRIMARY KEY,
-	room_id int,
-	client_id,
-	row Drows,
-	column DColumn,
-	isTaken tinyint,
-	CONSTRAINT chkRow CHECK (row between 1 and 20),
-	CONSTRAINT chkColumn CHECK (column between 1 and 35),
-	CONSTRAINT chkIsTaken CHECK (isTaken in (0, 1)),
-	FOREIGN KEY (room_id) REFERENCES tbRooms (room_id) on update cascade,
-	FOREIGN KEY (client_id) REFERENCES tbClients (client_id) on update cascade
-);
+	FOREIGN KEY (movie_id) REFERENCES tbMovies(movie_id) ON UPDATE CASCADE
+)
 
 CREATE table tbClients (
 	client_id int auto_increment PRIMARY KEY,
 	seat_id int,
 	client_type_id int,
 	reduction double,
-	FOREIGN KEY (seat_id) REFERENCES tbSeats(seat_id) on update cascade,
-	FOREIGN KEY (client_type_id) REFERENCES tbClientsType(client_type_id) on update cascade
-);
+	FOREIGN KEY (seat_id) REFERENCES tbSeats(seat_id) ON UPDATE CASCADE,
+	FOREIGN KEY (client_type_id) REFERENCES tbClientsType(client_type_id) ON UPDATE CASCADE
+)
+
+CREATE table tbSeats (
+	seat_id int auto_increment PRIMARY KEY,
+	room_id int,
+	client_id int,
+	sRow int,
+	sColumn int,
+	isTaken tinyint,
+	CONSTRAINT chkRow CHECK (sRow between 1 and 20),
+	CONSTRAINT chkColumn CHECK (sColumn between 1 and 35),
+	CONSTRAINT chkIsTaken CHECK (isTaken in (0, 1)),
+	FOREIGN KEY (room_id) REFERENCES tbRooms(room_id) ON UPDATE CASCADE,
+	FOREIGN KEY (client_id) REFERENCES tbClients(client_id) ON UPDATE CASCADE
+)
 
 CREATE table tbClientsType (
 	client_type_id int auto_increment PRIMARY KEY,
 	client_type CHAR(30)
-);
+)
