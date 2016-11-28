3fc50188743484965e28fa7930e4b1585a86ec60
Mathieu-R <mathieu0709@gmail.com>
2016-11-28 10:55:59 +0100

    model and db

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/eb/e0ef6acf4db50016140598006c6b4197
@@ -0,0 +1,19 @@
+package src.konopolis.model;
+
+import java.util.Observable;
+
+/**
+ * @author Mathieu R. - Groupe 3
+ */
+
+/**
+ * Model
+ * Abstract Class
+ * Implements Observer
+ * It informs the modifications to the view
+ */
+public class KonopolisModel extends Observable {
+
+    private DB db = null;
+
+}
---
+++ src/konopolis/model/Seat.java
@@ -0,0 +1,103 @@
+package src.konopolis.model;
+
+/**
+ * @author Mathieu R. - Groupe 3
+ */
+
+public class Seat {
+
+    /**
+     * row, row of the sit
+     * column, column of the sit in the row
+     * isTaken, boolean, to know if the sit is taken
+     */
+    private int row;
+    private int column;
+    private boolean isTaken;
+    private Customer customer;
+
+    /**
+     * constructor
+     * @param row, row of the sit
+     * @param column, column of the sit in the row
+     * By default, the sit is available
+     */
+    public Seat(int row, int column) {
+        this.row = row;
+        this.column = column;
+        this.isTaken = false;
+    }
+
+    /**
+     * Getters and Setters
+     */
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public boolean isTaken() {
+        return isTaken;
+    }
+
+    public void setTaken(boolean taken) {
+        isTaken = taken;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    /**
+     * HashCode and Equals
+     */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Seat sit = (Seat) o;
+
+        if (row != sit.row) return false;
+        return column == sit.column;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = row;
+        result = 31 * result + column;
+        return result;
+    }
+
+    /**
+     * toString
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "Seat{" +
+                "row=" + row +
+                ", column=" + column +
+                ", isTaken=" + isTaken +
+                ", customer=" + customer +
+                '}';
+    }
+}
