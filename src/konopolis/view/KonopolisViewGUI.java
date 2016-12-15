3fc8c6539739999be91938c314812c18051c25b7
Mathieu-R <mathieu0709@gmail.com>
2016-12-15 17:51:09 +0100

    button event

--- src/konopolis/view/KonopolisViewGUI.java
+++ src/konopolis/view/KonopolisViewGUI.java
@@ -11,6 +11,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Map;
@@ -42,6 +44,7 @@ public class KonopolisViewGUI extends KonopolisView implements Observer {
 	private String enteredType = "";
 	private LocalDateTime show_start;
 	private ArrayList<String> listTitles = new ArrayList<String>();
+	private Room selectedRoom;
 	private String [] shows = new String [50];
 	
 	
@@ -146,7 +149,7 @@ public class KonopolisViewGUI extends KonopolisView implements Observer {
 		panelDisplay.setBackground(Color.WHITE);
 		panelDisplay.setBorder(BorderFactory.createLineBorder(Color.black));
 		panelDisplay.setVisible(true);
-		panelDisplay.setMinimumSize(500, 500);
+		panelDisplay.setMinimumSize(new Dimension(500, 500));
 				
 		//Define panelInfo
 				
@@ -164,71 +167,93 @@ public class KonopolisViewGUI extends KonopolisView implements Observer {
         displayShows();
 		
 	}
-		/**
-		 * Fill the ComboBox of Shows
-		 */
-		private void displayShows() {
-			//System.out.println(moviesList.getSelectedItem() + " - " + moviesList.getSelectedIndex());
-			String choiceMovie = (String) moviesList.getSelectedItem();
-			int idMovie = control.retrieveMovieId(choiceMovie);
-			System.out.println(idMovie);
-			Movie movie = control.retrieveMovie(idMovie);
-			showsList.removeAllItems();
-			infos.setText(
-			        movie.getDescription() +
-                    " Prix: " + movie.getPrice()
-            );
-			
-			for(Show sh: movie.getShows()){
-				showsList.addItem("Salle n°" + sh.getRoom_id() + " - " + control.dateInFrench(sh.getShow_start()));
-			} 		
-			//Select the first item of the list
-			showsList.setSelectedIndex(0);
-			showsList.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent eventSource) {
-					displayRoom();
-			   }
-		    });
-		}
-		/**
-		 * Display the room at the moment of the show
-		 */
-		private void displayRoom() {
-			Show selectedShow = control.getShows_al().get(showsList.getSelectedIndex());
-			System.out.println(showsList.getSelectedIndex());
-			Room selectedRoom = control.retrieveRoom(selectedShow.getMovie_id(), selectedShow.getRoom_id(), selectedShow.getShow_start());
-			panelDisplay = new JPanel(new GridLayout(selectedRoom.getRows(), selectedRoom.getSeatsByRow()));
+    /**
+     * Fill the ComboBox of Shows
+     */
+    private void displayShows() {
+        //System.out.println(moviesList.getSelectedItem() + " - " + moviesList.getSelectedIndex());
+        String choiceMovie = (String) moviesList.getSelectedItem();
+        int idMovie = control.retrieveMovieId(choiceMovie);
+        //System.out.println(idMovie);
+        Movie movie = control.retrieveMovie(idMovie);
+        showsList.removeAllItems();
+        infos.setText(
+                movie.getDescription() +
+                " Prix: " + movie.getPrice()
+        );
 
-            for (int i = 0; i < selectedRoom.getRows(); i++) {
-                for (int j = 0; j < selectedRoom.getSeatsByRow(); j++) {
-                    // Button
-                    JButton seatButton = new JButton(i + ", " + j);
-                    seatButton.setBorder(BorderFactory.createEmptyBorder());
-                    seatButton.setBorderPainted(false);
-                    if (selectedRoom.getSeats().get(i).get(j).isTaken()) {
-                        seatButton.setBackground(Color.decode("#e74c3c"));
-                    } else {
-                        seatButton.setBackground(Color.gray);
-                    }
-                    seatButton.setPreferredSize(new Dimension(75, 75));
-                    seatButton.setMargin(new Insets(0, 0, 0, 0));
-
-                    // Panel for buttons
-                    JPanel panelButtons = new JPanel();
-                    panelButtons.setPreferredSize(new Dimension(75, 75));
-                    panelButtons.setBorder(BorderFactory.createEmptyBorder());
-                    panelButtons.add(seatButton);
-
-                    // Add to the Map
-                    panelDisplay.add(panelButtons);
-                }
-            }
-            // Add the Map to the Jframe
-            frame.add(panelDisplay,BorderLayout.CENTER);
-		}
-
-        private void displayAuth() {
-            // TODO auth form
-            // if ok => config
+        for(Show sh: movie.getShows()){
+            showsList.addItem("Salle n°" + sh.getRoom_id() + " - " + control.dateInFrench(sh.getShow_start()));
         }
+        //Select the first item of the list
+        showsList.setSelectedIndex(0);
+        showsList.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent eventSource) {
+                displayRoom();
+           }
+        });
+    }
+
+    /**
+     * Display the room at the moment of the show
+     */
+    private void displayRoom() {
+        Show selectedShow = control.getShows_al().get(showsList.getSelectedIndex());
+        //System.out.println(showsList.getSelectedIndex());
+        selectedRoom = control.retrieveRoom(selectedShow.getMovie_id(), selectedShow.getRoom_id(), selectedShow.getShow_start());
+        panelDisplay = new JPanel(new GridLayout(selectedRoom.getRows(), selectedRoom.getSeatsByRow()));
+        //JButton[][] grid = new JButton[selectedRoom.getRows()][selectedRoom.getSeatsByRow()]; // grid of JButtons
+        for (int y = 0; y < selectedRoom.getRows(); y++) {
+            for (int x = 0; x < selectedRoom.getSeatsByRow(); x++) {
+                // Button
+                JButton seat = new JButton(x + ", " + y);
+                seat.setBorder(BorderFactory.createEmptyBorder());
+                seat.setBorderPainted(false);
+                if (selectedRoom.getSeats().get(y).get(x).isTaken()) {
+                    seat.setBackground(Color.decode("#e74c3c"));
+                } else {
+                    seat.setBackground(Color.gray);
+                }
+                seat.setPreferredSize(new Dimension(75, 75));
+                seat.setMargin(new Insets(0, 0, 0, 0));
+
+                // Event Listener
+                int finalX = x;
+                int finalY = y;
+                seat.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseEntered(MouseEvent e) {
+                        if (!(selectedRoom.getSeats().get(finalY).get(finalX).isTaken())) {
+                            seat.setBackground(Color.decode("#27ae60"));
+                        }
+                    }
+
+                    @Override
+                    public void mouseExited(MouseEvent e) {
+                        seat.setBackground(Color.gray);
+                        /*if(seat.getBackground().equals("#27ae60")) {
+                            seat.setBackground(Color.gray);
+                        }*/
+                    }
+                });
+
+                // Panel for buttons
+                JPanel panelButtons = new JPanel();
+                panelButtons.setPreferredSize(new Dimension(75, 75));
+                panelButtons.setBorder(BorderFactory.createEmptyBorder());
+                panelButtons.add(seat);
+
+                // Add to the Map
+                panelDisplay.add(panelButtons);
+            }
+        }
+        // Add the Map to the Jframe
+        frame.add(panelDisplay,BorderLayout.CENTER);
+        //frame.pack(); // set correct size for frame
+    }
+
+    private void displayAuth() {
+        // TODO auth form
+        // if ok => config
+    }
 }
